f4daeede2b6bcd30c88f0ef1d7f7c42f2226f4b9
escottalexander@gmail.com <escottalexander@gmail.com>
2024-09-10 09:47:47 -0400

    remove unused files, clean up logic

--- src/utils/render-intro-message.ts
+++ src/utils/render-intro-message.ts
@@ -1,11 +1,30 @@
 import chalk from "chalk";
 
-export const TITLE_TEXT = `
- ${chalk.bold.blue("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")}
- ${chalk.bold.blue("|     ETH Dev Tech Tree     |")}
- ${chalk.bold.blue("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")}
-`;
-
 export function renderIntroMessage() {
   console.log(TITLE_TEXT);
 }
+
+export const TITLE_TEXT = `
+${chalk.green(`●──┬──●──┬──●──┬──●──┬──●──┬──●──┬──●──┬──●──┬──●──┬──●──┬──●──┬──●──┬──●`)}
+${chalk.green(`   │     │     │     │     │     │     └──●  │     │     │     │     └──●`)}
+${chalk.green(`   │     │     │     │     │     │           │     │     │     │`)}
+${chalk.green(`   │     │     │     │     │     └──●──┬──●  │     │     │     └──●──┬──●`)}
+${chalk.green(`   │     │     │     │     │           └──●  │     │     │           │`)}
+${chalk.green(`   │     │     │     │     │                 │     │     │           └──●`)}
+${chalk.green(`   │     │     │     │     └──●──┬──●──┬──●  │     │     └──●──┬──●──┬──●`)}
+${chalk.green(`   │     │     │     │           │     └──●  │     └──●──┬──●  │     └──●`)}
+${chalk.green(`   │     │     │     │           └──●        │           └──●  └──●`)}
+${chalk.green(`   │${chalk.blue("▗▄▄▄▖▗▄▄▄▖▗▖ ▗▖")}  │ ${chalk.blue("▗▄▄▄▖▗▄▄▄▖ ▗▄▄▖▗▖ ▗▖")}  │ ${chalk.blue("▗▄▄▄▖▗▄▄▖ ▗▄▄▄▖▗▄▄▄▖")}──┬──●`)}
+${chalk.green(`   │${chalk.blue("▐▌     █  ▐▌ ▐▌")}  │ ${chalk.blue("  █  ▐▌   ▐▌   ▐▌ ▐▌")}  │ ${chalk.blue("  █  ▐▌ ▐▌▐▌   ▐▌   ")}  └──●`)}
+${chalk.green(`   │${chalk.blue("▐▛▀▀▘  █  ▐▛▀▜▌")}  │ ${chalk.blue("  █  ▐▛▀▀▘▐▌   ▐▛▀▜▌")}  │ ${chalk.blue("  █  ▐▛▀▚▖▐▛▀▀▘▐▛▀▀▘")}──┬──●`)}
+${chalk.green(`   │${chalk.blue("▐▙▄▄▖  █  ▐▌ ▐▌")}  │ ${chalk.blue("  █  ▐▙▄▄▖▝▚▄▄▖▐▌ ▐▌")}  │ ${chalk.blue("  █  ▐▌ ▐▌▐▙▄▄▖▐▙▄▄▖")}  └──●`)}
+${chalk.green(`   │     │     │     └──●                    │`)}
+${chalk.green(`   │     │     └──●──┬──●──┬──●──┬──●──┬──●  └───●─┬──●──┬──●──┬──●──┬──●`)}
+${chalk.green(`   │     └──●──┬──●  └──●  │     │     │           │     │     │     └──●`)}
+${chalk.green(`   │           └──●──┬──●  │     │     │           │     │     └──●`)}
+${chalk.green(`   │                 │     │     │     └──●──┬──●  │     └──●──┬──●`)}
+${chalk.green(`   └──●──┬──●──┬──●  └──●  │     │           │     │           └──●──┬──●`)}
+${chalk.green(`         │     │           │     │           └──●  │                 │`)}
+${chalk.green(`         └──●  └──●        │     │                 └──●──┬──●──┬──●  └──●`)}
+${chalk.green(`                           │     │                       │     └──●`)}
+`;
