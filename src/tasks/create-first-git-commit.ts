4d0e0e59f3bcc2d2f3a5775a70678802594d54a7
escottalexander@gmail.com <escottalexander@gmail.com>
2024-09-17 16:56:12 -0400

    after installing repo commit all changes as first commit

---
+++ src/tasks/create-first-git-commit.ts
@@ -0,0 +1,23 @@
+import { execa } from "execa";
+import path from "path";
+
+const foundryLibraries = ["foundry-rs/forge-std", "OpenZeppelin/openzeppelin-contracts", "gnsps/solidity-bytes-utils"];
+
+export async function createFirstGitCommit(targetDir: string) {
+  try {
+    await execa("git", ["add", "-A"], { cwd: targetDir });
+    await execa("git", ["commit", "-m", "Initial commit with 🏗️ Scaffold-ETH 2", "--no-verify"], { cwd: targetDir });
+    const foundryWorkSpacePath = path.resolve(targetDir, "packages", "foundry");
+    // remove lib directory since it doesn't like directories to exist when installing
+    await execa("rm",["-rf", "lib"], { cwd: foundryWorkSpacePath });
+    // forge install foundry libraries
+    await execa("forge", ["install", ...foundryLibraries, "--no-commit"], { cwd: foundryWorkSpacePath });
+    await execa("git", ["add", "-A"], { cwd: targetDir });
+    await execa("git", ["commit", "--amend", "--no-edit"], { cwd: targetDir });
+  } catch (e: any) {
+    // cast error as ExecaError to get stderr
+    throw new Error("Failed to initialize git repository", {
+      cause: e?.stderr ?? e,
+    });
+  }
+}
