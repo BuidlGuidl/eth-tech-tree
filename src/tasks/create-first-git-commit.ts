d1fbe1706c8652ac4f8d60376562dea032e65fc3
escottalexander@gmail.com <escottalexander@gmail.com>
2024-10-01 10:06:30 -0400

    add logging

--- src/tasks/create-first-git-commit.ts
+++ src/tasks/create-first-git-commit.ts
@@ -4,20 +4,31 @@ import path from "path";
 const foundryLibraries = ["foundry-rs/forge-std", "OpenZeppelin/openzeppelin-contracts", "gnsps/solidity-bytes-utils"];
 
 export async function createFirstGitCommit(targetDir: string) {
+  let errorLog = "";
   try {
     const foundryWorkSpacePath = path.resolve(targetDir, "packages", "foundry");
+
+    errorLog = "Removing lib directory";
     // remove lib directory since it doesn't like directories to exist when installing
     await execa("rm",["-rf", "lib"], { cwd: foundryWorkSpacePath });
+
+    errorLog = "Installing forge dependencies";
     // forge install foundry libraries
     await execa("forge", ["install", ...foundryLibraries, "--no-commit"], { cwd: foundryWorkSpacePath });
+
+    errorLog = "git add -A";
     // Add and commit all changes
     await execa("git", ["add", "-A"], { cwd: targetDir });
+
+    errorLog = "git commit  -m 'Initial commit with 🏗️ Scaffold-ETH 2'";
     await execa("git", ["commit", "-m", "Initial commit with 🏗️ Scaffold-ETH 2", "--no-verify"], { cwd: targetDir });
+    
+    errorLog = "git remote remove origin";
     // Remove remote
     await execa("git", ["remote", "remove", "origin"], { cwd: targetDir });
   } catch (e: any) {
     // cast error as ExecaError to get stderr
-    throw new Error("Failed to initialize git repository", {
+    throw new Error(`Failed to initialize git repository after step: ${errorLog}`, {
       cause: e?.stderr ?? e,
     });
   }
